0c3b282f0afad6c3dba62ebf17085a143c41f36d
rvpanoz <rvpanoz@gmail.com>
2019-10-26 23:36:12 +0300

    Add state and click event

--- src/index.js
+++ src/index.js
@@ -3,13 +3,77 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-// TODO: Change component so when clicked it updates its state and draws an 'X'
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(squareIndex) {
+    const squares = this.state.squares.slice();
+
+    squares[squareIndex] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  renderSquare(squareIndex) {
+    return (
+      <Square
+        value={this.state.squares[squareIndex]}
+        onClick={() => this.handleClick(squareIndex)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
 }
 
 // ========================================
 
-ReactDOM.render(<Square />, document.getElementById("root"));
+ReactDOM.render(<Board />, document.getElementById("root"));
