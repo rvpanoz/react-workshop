a58e319b7d7579d44948d02d26d210485c686624
rvpanoz <rvpanoz@gmail.com>
2019-10-26 23:38:27 +0300

    List state

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
@@ -73,6 +65,22 @@ class Board extends React.Component {
     );
   }
 }
+
+// class Game extends React.Component {
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className="game-info">
+//           <div>{/* status */}</div>
+//           <ol>{/* TODO */}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 // ========================================
 
