57f93f1a1fbd4f941fd437dedfe220c20ccb6b92
rvpanoz <rvpanoz@gmail.com>
2019-10-26 23:28:26 +0300

    Create simple React component

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+// TODO: Create a simple React Component named 'Square' and add class named 'square' to it.
+
+
+// ========================================
+
+ReactDOM.render(
+    null, // HINT: You should render your 'Square' component here
+    document.getElementById('root')
+);
