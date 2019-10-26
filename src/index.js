3f80a87179a2064ce63cc9e3e43a443c64b64457
rvpanoz <rvpanoz@gmail.com>
2019-10-27 00:34:01 +0300

    lift state and events

--- src/index.js
+++ src/index.js
@@ -14,31 +14,11 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    };
-  }
-
-  handleClick(squareIndex) {
-    const squares = this.state.squares.slice();
-
-    squares[squareIndex] = this.state.xIsNext ? "X" : "O";
-
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   renderSquare(squareIndex) {
     return (
       <Square
-        value={this.state.squares[squareIndex]}
-        onClick={() => this.handleClick(squareIndex)}
+        value={this.props.squares[squareIndex]}
+        onClick={() => this.props.onSquareClick(squareIndex)}
       />
     );
   }
@@ -66,22 +46,79 @@ class Board extends React.Component {
   }
 }
 
-// class Game extends React.Component {
-//   render() {
-//     return (
-//       <div className="game">
-//         <div className="game-board">
-//           <Board />
-//         </div>
-//         <div className="game-info">
-//           <div>{/* status */}</div>
-//           <ol>{/* TODO */}</ol>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+// TODO: Use lifecycle method componentDidMount to load state from localstorage
+// TODO: Use lifecycle method componentDidUpdate to check if a winner exists and update localstorage
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+
+    this.player1InputRef = React.createRef();
+    this.player2InputRef = React.createRef();
+  }
+
+  handleClick(squareIndex) {
+    const squares = this.state.squares.slice();
+
+    squares[squareIndex] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  addPlayers() {
+    const player1 = this.player1InputRef.current.value;
+    const player2 = this.player1InputRef.current.value;
+
+    this.setState({
+      player1,
+      player2
+    });
+  }
+
+  componentDidMount() {
+    // TODO: implementation
+  }
+
+  componentDidUpdate() {
+    // TODO: implementation
+  }
+
+  render() {
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={this.state.squares} onSquareClick={(squareIndex) => this.handleClick(squareIndex)} />
+        </div>
+        <div>{status}</div>
+        <form>
+          <div className="wrapper">
+            <div className="form-row">
+              <label htmlFor="player-1">Player 1</label>
+              <input type="text" id="player-1" ref={this.player1InputRef} />
+            </div>
+            <div className="form-row">
+              <label htmlFor="player-2">Player 2</label>
+              <input type="text" id="player-2" ref={this.player2InputRef} />
+            </div>
+            <div className="form-row">
+              <button type="button" onClick={() => this.addPlayers()}>Add players</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 // ========================================
 
-ReactDOM.render(<Board />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
